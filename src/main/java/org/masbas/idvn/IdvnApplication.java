5bf980b7b4dbd3a818d94975442eee1d0d7b630f
masbas <masbas@masbass-MacBook-Pro.local>
2020-12-01 10:23:38 +0700

    Update spring config

--- src/main/java/org/masbas/idvn/IdvnApplication.java
+++ src/main/java/org/masbas/idvn/IdvnApplication.java
@@ -3,11 +3,18 @@ package org.masbas.idvn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class IdvnApplication extends SpringBootServletInitializer  {
 
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder 
+	application) {
+	    return application.sources(IdvnApplication.class);
+	   }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(IdvnApplication.class, args);
 	}
